797106969b92e041fc75bbc7cdb9a6f1fc75b3a4
Neil-Do <dotatthanhk60@gmail.com>
2019-11-28 01:33:23 +0700

    Update code, vietnamese dataset

--- LGSentiment.py
+++ LGSentiment.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.datasets import load_files
 
+
 reviews_train = load_files("Dataset/aclImdb/train/")
 text_train, label_train = reviews_train.data, reviews_train.target
 
@@ -14,6 +15,7 @@ print("Samples per class (test): {}".format(np.bincount(label_test)))
 
 
 """
+"""
 from sklearn.feature_extraction.text import CountVectorizer
 vect = CountVectorizer(min_df=5, ngram_range=(2, 2))
 X_train = vect.fit(text_train).transform(text_train)
@@ -25,4 +27,17 @@ print("X_test: \n{}".format(repr(X_test)))
 
 feature_names = vect.get_feature_names()
 print("Number of features: {}".format(len(feature_names)))
-"""
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
+grid = GridSearchCV(LogisticRegression(), param_grid, n_jobs=-1, pre_dispatch=16, cv=5)
+grid.fit(X_train, label_train)
+
+print("Best cross-validation score: {:.2f}".format(grid.best_score_))
+print("Best parameters: ", grid.best_params_)
+print("Best estimator: ", grid.best_estimator_)
+
+lr = grid.best_estimator_
+lr.predict(X_test)
+print("Score: {:.2f}".format(lr.score(X_test, label_test)))
